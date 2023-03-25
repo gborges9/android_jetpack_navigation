ec11904df81fb8e070798eb101a3981bc4c46a63
Gabriel <gborges9@outlook.com>
2023-03-25 17:14:15 -0300

    5. Navigating Between Destinations - complete

---
+++ app/src/main/java/com/gborges9/navigation_example/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.gborges9.navigation_example
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_home, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.navigate_to_page_btn).setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_pageFragment)
+        }
+    }
+}
